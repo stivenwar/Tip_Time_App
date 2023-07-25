084b3035abc59712b94b7e2f8514111a206c5e51
esteban <estebanguerra1990@gmail.com>
2023-07-25 15:08:18 +0200

    tips App example

--- app/src/main/java/com/stivenwar/tiptime/MainActivity.kt
+++ app/src/main/java/com/stivenwar/tiptime/MainActivity.kt
@@ -11,7 +11,7 @@ import java.text.NumberFormat
 import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
-
+    //stivenWar
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
